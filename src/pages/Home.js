a49224604603d06914b0faca0d55940c0c2ca077
Tayomide <tibrahim@hawk.iit.edu>
2022-12-31 16:17:55 -0600

    Create Home Page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,23 @@
 import styled from "styled-components"
+import { Background } from "../components/Home/Background"
+import { Content } from "../components/Home/Content"
+import { Footer } from "../components/Home/Footer"
+import { Header } from "../components/Home/Header"
 
 export const Home = () => {
   return (
     <Container>
-      Home
+      <Background backgroundUrl={"/background1.svg"} cover/>
+      <Background backgroundUrl={"/background2.svg"} />
+      <Header />
+      <Content />
+      <Footer />
     </Container>
   )
 }
 
-const Container = styled.div``
+const Container = styled.div`
+  background: radial-gradient(50% 100%,#180526 0,#040e21 100%);
+  height: 100%;
+  position: relative;
+`
