42e0738d16a778f028d1a3e1a4b6dbfaa3ff3868
Tayomide <tibrahim@hawk.iit.edu>
2022-12-31 16:17:06 -0600

    Create Footer component

---
+++ src/components/Home/Footer.js
@@ -0,0 +1,49 @@
+import { Link } from 'react-router-dom'
+import React from 'react'
+import styled from 'styled-components'
+
+export const Footer = () => {
+  return (
+    <Container>
+      <ul>
+        <li>
+          <p>Made with ♥ by GitHub</p>
+          <p className='blur'>VR Ready</p>
+        </li>
+        <li>
+          <Link to="/">Terms</Link>
+          <Link to="/">Privacy</Link>
+          <Link to="/">FAQ</Link>
+        </li>
+      </ul>
+    </Container>
+  )
+}
+
+const Container = styled.footer`
+  ul{
+    align-items: flex-start;
+    display: flex;
+    flex-direction: row;
+    height: 3em;
+    justify-content: space-between;
+    list-style: none;
+    padding: 0 3.5em;
+    position: relative;
+    li{
+      color: white;
+      display: flex;
+      flex-direction: row;
+      gap: 1em;
+      a, p{
+        color: inherit;
+      }
+      a{
+        text-decoration: none;
+      }
+      .blur{
+        color: #B5B6BC;
+      }
+    }
+  }
+`
