9a02304c0db4523c17195f678428ab0a6343b864
Tayomide <tibrahim@hawk.iit.edu>
2022-12-31 16:17:33 -0600

    Create Header component

---
+++ src/components/Home/Header.js
@@ -0,0 +1,25 @@
+import styled from "styled-components"
+import GitHubIcon from '@mui/icons-material/GitHub';
+import { Link } from "react-router-dom";
+
+export const Header = () => {
+  return (
+    <Container to="/"><GitHubIcon sx={{width:36, height:36}}/>Resist Desist</Container>
+  )
+}
+
+const Container = styled(Link)`
+  align-items: center;
+  color: #FFFFFF;
+  display: inline-flex;
+  flex-direction: row;
+  font-size: 1.55em;
+  font-weight: bold;
+  left: 3.5rem;
+  position: absolute;
+  text-decoration: none;
+  top: 3rem;
+  svg{
+    margin-right: 0.28em
+  }
+`
