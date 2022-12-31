aa6af9ab3d53834bfe8b64854d241b9f77e32a13
Tayomide <tibrahim@hawk.iit.edu>
2022-12-31 16:18:09 -0600

    Add Home page to React App

--- src/App.js
+++ src/App.js
@@ -1,19 +1,14 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import styled from "styled-components"
 import { Home } from './pages/Home';
 
 function App() {
   return (
-    <Container>
-      <Router>
-        <Routes>
-          <Route exact path="/" element={<Home />} />
-        </Routes>
-      </Router>
-    </Container>
+    <Router>
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+      </Routes>
+    </Router>
   );
 }
 
 export default App;
-
-const Container = styled.div``
